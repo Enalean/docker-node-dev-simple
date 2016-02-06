8ffa7fd9c584164bdf18b19eac5f3f5d59a799f9
Thomas Gerbet <thomas.gerbet@enalean.com>
2016-02-06 18:04:23 +0100

    Remove useless example

--- example/hello-world.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var express = require('express');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello world');
-}).listen(80);
